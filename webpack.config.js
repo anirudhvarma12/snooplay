acb4c1e7955719935173737353b8b55c004a18c0
Anirudh Varma <varma93@hotmail.com>
2017-01-21 15:24:25 +0530

    Add config to copy files to docs to host on Github

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 var webpack = require('webpack');
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'src');
+//For GitHub pages
+var GH_DIR = path.resolve(__dirname, 'docs');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
@@ -25,7 +28,13 @@ var config = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: BUILD_DIR + "/bundle.js", to: GH_DIR + "/build" },
+      { from: APP_DIR + "/*.html", to: GH_DIR },
+      { from: APP_DIR + "/*.css", to: GH_DIR },
+      { from: APP_DIR + "/icons", to: GH_DIR + "/src/icons/" }
+    ])
   ]
 };
 
